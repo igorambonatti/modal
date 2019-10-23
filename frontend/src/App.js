a8cc9141d23301c3468c9f1870c04d5c06b35cdb
unknown <contatoambonatti@gmail.com>
2019-10-23 01:23:35 -0300

    inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+import Routes from './routes';
+
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <div className="container">
+      <a href="/"><img src={logo} alt="AirCnc" /></a>
+      <div className="content">
+        <Routes/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
